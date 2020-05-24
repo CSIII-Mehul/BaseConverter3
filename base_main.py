234e5696d9858f8f97edbadffc9f6ff4e8e1b6ef
Mehul <mehul@example.com>
2020-05-24 12:07:00 -0500

    done

--- base_main.py
+++ base_main.py
@@ -17,6 +17,39 @@ def alter_list(arr):
 
 def converter(base_initial,num,base_converted):
 
+    if base_initial != 10:
+       to10 = 0
+       counter = len(num)-1
+       index = 0
+       while counter > -1:
+           to10 += int(num[counter]) * base_initial ** index
+           counter -=1
+           index +=1
+
+       if base_converted ==10:
+           return to10
+       elif base_converted < 10:
+           if to10 < base_converted:
+               return to10
+           else:
+              return modulus(to10, base_converted)
+       elif base_converted > 10 :
+             return modulus(to10, base_converted)
+
+
+    elif base_initial == 10:
+        if base_converted == 10:
+            return num
+        elif base_converted < 10:
+           if int(num) < base_converted:
+               return int(num)
+           else:
+              return modulus(int(num), base_converted)
+        elif base_converted > 10:
+            return modulus(int(num), base_converted)
+
+
+
     return 0
 
 def valid_number(num, base_initial):
@@ -52,7 +85,51 @@ def valid_number(num, base_initial):
 
     return True
                 
-      
+def modulus(num, base):
+    new_num = ""
+    num = num
+    if base < 11:
+     while num >= base:
+        new_num += str(num % base)
+        num = num // base
+        if num < base:
+            new_num += str(num)
+            break
+    else:
+        if num < base:
+            if num >=10:
+                return alpha_change(num)
+        while num >= base:
+            if (num % base) >=10:
+                new_num += str(alpha_change(num % base))
+            else:
+               new_num += str(num % base)
+            num = num // base
+            if num < base:
+                 if (num) >=10:
+                       new_num += str(alpha_change(num))
+                 else:
+                      new_num += str(num)
+                 break
+
+
+   
+    return new_num[::-1]
+
+def alpha_change(num):
+    if num == 10:
+       return "A"
+    if  num ==  11:
+         return "B"
+    if num ==  12:
+         return "C"
+    if num ==  13:
+       return "D"
+    if num ==  14:
+       return "E"
+    if num ==  15: 
+        return "F"
+
 def run():
 
     base_initial = int(input("what base are you starting with?"))
@@ -67,8 +144,8 @@ def run():
     while base_converted < 0 or base_converted> 16: 
             base_converted = int(input("what base do you want to convert to?"))
     
-    converter(base_initial,num,base_converted)
-    #print(alter_list([1,2,3]))
+    print(converter(base_initial,num,base_converted))
+    
 
    
     return 0
