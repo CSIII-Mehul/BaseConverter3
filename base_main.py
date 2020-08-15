ed3a31f3a49746b8bf5dce2720d7e06903fa9f44
Mehul623248 <mehul.murali@gmail.com>
2020-08-14 23:07:30 -0500

    fixed some readibility issues and one logic issue

--- base_main.py
+++ base_main.py
@@ -1,6 +1,6 @@
 import math
 
-
+'''
 def alter_list(arr):
     alter = []
     index = []
@@ -15,9 +15,12 @@ def alter_list(arr):
 
     return alter
 
+ '''   
+
 def converter(base_initial,num,base_converted):
 
     if base_initial != 10:
+       #variable to10 is used to convert numbers that weren't initially in base 10 to base 10 for easier conversion
        to10 = 0
        counter = len(num)-1
        index = 0
@@ -53,32 +56,19 @@ def converter(base_initial,num,base_converted):
     return 0
 
 def valid_number(num, base_initial):
-   #while loop below looks to see if inputted number is valid given original base
+   #while loop below looks to see if inputted number is valid given original base, 
+   # Ex: if you say you are starting with base 2 this function makes sure that digits greater than 2 are not allowed
+    alpha_dict= {
+        10:"A", 11:"B", 12: "C", 13: "D", 14: "E", 15: "F" 
+    }
     check = base_initial
     while check < 16:
         for i in range(len(num)):
             if check >= 10:
-               if check == 10:
-                    if "A" in num:
+               if check in alpha_dict:
+                    if alpha_dict[check] in num:
                         return False
-               if check ==  11:
-                    if "B" in num:
-                        return False
-               if check ==  12:
-                    if "C" in num:
-                        return False
-               if check ==  13:
-                     if "D" in num:
-                        return False
-               if check ==  14:
-                    if "E" in num:
-                        return False
-               if check ==  15:
-                    if "F" in num:
-                        return False
-
-            else:
-                if str(check) in num:
+            elif str(check) in num:
                     return False
 
         check+=1
@@ -87,7 +77,6 @@ def valid_number(num, base_initial):
                 
 def modulus(num, base):
     new_num = ""
-    num = num
     if base < 11:
      while num >= base:
         new_num += str(num % base)
@@ -104,6 +93,7 @@ def modulus(num, base):
                 new_num += str(alpha_change(num % base))
             else:
                new_num += str(num % base)
+
             num = num // base
             if num < base:
                  if (num) >=10:
@@ -117,37 +107,35 @@ def modulus(num, base):
     return new_num[::-1]
 
 def alpha_change(num):
-    if num == 10:
-       return "A"
-    if  num ==  11:
-         return "B"
-    if num ==  12:
-         return "C"
-    if num ==  13:
-       return "D"
-    if num ==  14:
-       return "E"
-    if num ==  15: 
-        return "F"
 
-def run():
-
-    base_initial = int(input("what base are you starting with?"))
-    while base_initial < 0 or base_initial> 16: 
-            base_initial = int(input("what base are you starting with?"))
-
-    num = input("what number are you converting form that base?")
-    while valid_number(num,base_initial) == False:
-        num = input("what number are you converting form that base?")    
-
-    base_converted = int(input("what base do you want to convert to?"))
-    while base_converted < 0 or base_converted> 16: 
-            base_converted = int(input("what base do you want to convert to?"))
-    
-    print(converter(base_initial,num,base_converted))
-    
-
+    alpha_dict= {
+        10:"A", 11:"B", 12: "C", 13: "D", 14: "E", 15: "F" 
+    }
+    if num in alpha_dict:
+        return alpha_dict[num]
    
+def run():
+    try:
+        base_initial = int(input("what base are you starting with?"))
+        while base_initial < 2 or base_initial> 16: 
+                base_initial = int(input("what base are you starting with?"))
+
+        num = input("what number are you converting from that base?")
+        while valid_number(num,base_initial) == False:
+            num = input("what number are you converting form that base?")    
+
+        base_converted = int(input("what base do you want to convert to?"))
+        while base_converted < 2 or base_converted> 16: 
+                base_converted = int(input("what base do you want to convert to?"))
+
+
+        print(converter(base_initial,num,base_converted))
+    except ValueError:
+        print("rerun the program and type in integers only")
+
+    
+    
+
     return 0
 
 
